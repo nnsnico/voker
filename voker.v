c14d257c9e76a4b9c594756abeec734b87d67f17
nnsnico <y_toyama@fancs.com>
2022-08-28 03:19:34 +0900

    refactoring code

--- voker.v
+++ voker.v
@@ -1,16 +1,8 @@
 module main
 
-import card
 import poker
-import rand
 
 fn main() {
-	println('------------------')
-	println('-- simple poker --')
-	println('------------------')
-
-	mut deck := rand.shuffle_clone(card.all_cards())?
-	hand := poker.to_hand(&deck[..5])?
-	println(deck)
-	println(hand)
+	mut deck := poker.setup_deck()?
+	poker.start(mut deck)?
 }
